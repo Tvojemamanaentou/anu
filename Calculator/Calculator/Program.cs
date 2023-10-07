a7ed20a9919fd3884cb40e2000c6cedf8e1bad84
Tvojemamanaentou <cipherbill197@gmail.com>
2023-10-07 14:33:31 +0200

    Kalkulajda hotová,plno funkcí, snad vše funguje, snad za jedna, spíš ne, fuck me....

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -15,43 +15,25 @@ namespace Calculator
     {
         static void HelpPrikaz()
         {
-            Console.WriteLine("\nSEZNAM PŘÍKAZŮ PRO ISIINU PRĎÁCKOU KALKULAČKU!\n\n\nPŘÍMÉ PŘÍKAZY-\n  help: hádej vole;\n  odečti: odečte dvě zadaná čísla;\n  sečti: sečte dvě zadaná čísla;\n  znásob: znásobí dvě zadaná čísla;\n  vyděl: vydělí dvě zadaná čísla;\n\nOPERATIVNÍ PŘÍKAZY-\n  nadruhou: zadáno-li po výběru čísla, znásobí číslo samo se sebou;\n  odmocni: zadáno-li po výběru čísla, nastaví číslo jako odmocninu sama sebe; \n\n A teď zadej druhé číslo, nebo operativní příkaz");
+            Console.WriteLine("\nSEZNAM PŘÍKAZŮ PRO ISIINU PRĎÁCKOU KALKULAČKU!\n\n\nPŘÍMÉ PŘÍKAZY-\n  help: hádej vole;\n  odečti: odečte dvě zadaná čísla;\n  sečti: sečte dvě zadaná čísla;\n  znásob: znásobí dvě zadaná čísla;\n  vyděl: vydělí dvě zadaná čísla;\n  procenta: ukáže kolik procent tvoří první číslo z druhého;\n  loga(b): vytvoří logaritmus z čísla jedna o základu čísla dva.\n\nOPERATIVNÍ PŘÍKAZY-\n  nadruhou: zadáno-li po výběru čísla, znásobí číslo samo se sebou;\n  odmocni: zadáno-li po výběru čísla, nastaví číslo jako odmocninu sama sebe;\n  ln:vytvoří přirozený logaritmus z posledního zadaného čísla;\n  log10:vytvoří logaritmus o zákaldu deset z posledního zadaného čísla;  \n\n A teď zadej druhé číslo, nebo operativní příkaz");
         }
         static void Main(string[] args)
         {
-            /* STRUKTURA:
-             * 
-             * Definice potřebných promněých
-             * představení programu
-             * 
-             * input prvního čísla
-             *      jestli je help, print help a opakujeme žádost
-             *      
-             *      
-             * input druhého čísla
-             *      jestli je help, print help a opakujeme žádost
-             *      jestli je odmocni/nadruhou, efektujeme 1. číslo a opakujeme žádost o input 2. čísla
-             *      
-             * input operace
-             *      příkazy, včetně pojistky dělění nulou
-             *          jestli je help, print help a opakujeme žádost
-             *          jestli je odmocni/nadruhou, efektujeme 2. číslo a opakujeme žádost o input příkazu
-             * 
-             * ReadKey
-             * KONEC PROGRAMU
-             */
             bool jeToInput;
             string input;
             float numberOne = 0;
+            double doubleEquivalentOne = 0;
+            double doubleEquivalentTwo = 0;
             float numberTwo = 0;
             int counter = 0;
             float result = 0;
-            int krok = 0;
-            int baseChange = 0;
+            int bases = 0;
+            int storage = 0;
 
             Console.WriteLine("ISIINA PRĎÁCKÁ KALKUALČKA\nZadej první číslo.\n");
             while (counter < 4)
             {
+
                 if (counter == 0)
                 {
                     input = Console.ReadLine(); //nastaví INPUT na to co napíše user
@@ -84,30 +66,37 @@ namespace Calculator
                     }
                     else
                     {
-                        if (input == "help")
+                        switch (input)
                         {
-                            HelpPrikaz();
-                        }
-                        else if (input == "nadruhou")
-                        {
-                            numberOne = numberOne * numberOne;
-                            Console.WriteLine("\nPrvní číslo bylo dáno nadruhou. Číslo nyní činí: " + numberOne + "\nZadejte druhé číslo, nebo operativní příkaz.\n");
-                        }
-                        else if (input == "odmocni")
-                        {
-                            numberOne = (float)Math.Sqrt(numberOne);
-                            Console.WriteLine("\nPrvní číslo bylo odmocněno. Číslo nyní činí: " + numberOne + "\nZadejte činnost druhé číslo, nebo operativní příkaz.\n");
-                        }
-                        else
-                        {
-                            Console.WriteLine("\nChuligáne! Tomu říkáš číslo!? Nemel blbosti a napiš něco pořádného!" + input + " konvertováno nebylo. \n"); //vybídka ke znovunastavení hodnoty uživatelem
+                            case "help":
+                                HelpPrikaz();
+                                break;
+                            case "nadruhou":
+                                numberOne = numberOne * numberOne;
+                                Console.WriteLine("\nPrvní číslo bylo dáno nadruhou. Číslo nyní činí: " + numberOne + "\nZadejte druhé číslo, nebo operativní příkaz.\n");
+                                break;
+                            case "odmocni":
+                                numberOne = (float)Math.Sqrt(numberOne);
+                                Console.WriteLine("\nPrvní číslo bylo odmocněno. Číslo nyní činí: " + numberOne + "\nZadejte druhé číslo, nebo operativní příkaz.\n");
+                                break;
+                            case "ln":
+                                numberOne = (float)Math.Log(numberOne);
+                                Console.WriteLine("\nPrvní číslo bylo překonvertováno na přirozený logaritmus. Číslo nyní činí: " + numberOne + "\nZadejte druhé číslo, nebo operativní příkaz.\n");
+                                break;
+                            case "log10":
+                                numberOne = (float)Math.Log10(numberOne);
+                                Console.WriteLine("\nPrvní číslo bylo překonvertováno na jeho logaritmus 10. Číslo nyní činí: " + numberOne + "\nZadejte druhé číslo, nebo operativní příkaz.\n");
+                                break;
+                            default:
+                                Console.WriteLine("\nChuligáne! Tomu říkáš číslo!? Nemel blbosti a napiš něco pořádného!" + input + " konvertováno nebylo. \n");
+                                break;
                         }
                     }
                 }
                 if (counter == 2)
                 {
                     input = Console.ReadLine(); //nastaví INPUT na to co napíše user
-                    switch (input)//nejspíš nejvýznamější část celé kalkulačky
+                    switch (input)//nejspíš nejvýznamější část celé kalkulačky!!! dělá jednotlivé operace
                     {
                         case "sečti":
                             result = numberOne + numberTwo;
@@ -137,7 +126,7 @@ namespace Calculator
                                 counter++;
                             }
                             break;
-                        case "poděl":
+                        case "poděl": //To samé jako vyděl, ale velice funni
                             if (numberTwo == 0)
                             {
                                 Console.WriteLine("\nNulou dělit nelze. Vyber nové druhé číslo.\n");
@@ -150,6 +139,16 @@ namespace Calculator
                                 counter++;
                             }
                             break;
+                        case "procenta":
+                                result = (numberOne / numberTwo) * 100;
+                                Console.WriteLine("\n Výsledek činní: " + result + " %");
+                            break;
+                        case "loga(b)":
+                                doubleEquivalentOne = Convert.ToDouble(numberOne);
+                                doubleEquivalentTwo = Convert.ToDouble(numberTwo);
+                                result = (float)Math.Log(doubleEquivalentOne, doubleEquivalentTwo); //logaritmus z čísla jedna o zákaldu dva
+                                Console.WriteLine("\n Výsledek činní: " + result);
+                            break;
                         case "help":
                             HelpPrikaz();
                             break;
@@ -161,6 +160,14 @@ namespace Calculator
                             numberTwo = (float)Math.Sqrt(numberTwo);
                             Console.WriteLine("\nDruhé číslo bylo odmocněno. Číslo nyní činí: " + numberTwo + "\nZadejte činnost (e.g. sečti, znásob, etc.), nebo operativní příkaz.\n");
                             break;
+                        case "ln":
+                            numberTwo = (float)Math.Log(numberTwo);
+                            Console.WriteLine("\nDruhé číslo bylo překonvertováno na přirozený logaritmus. Číslo nyní činí: " + numberTwo + "\nZadejte činnost (e.g. sečti, znásob, etc.), nebo operativní příkaz.\n");
+                            break;
+                        case "log10":
+                            numberTwo = (float)Math.Log10(numberTwo);
+                            Console.WriteLine("\nDruhé číslo bylo překonvertováno na logaritmus deseti. Číslo nyní činí: " + numberTwo + "\nZadejte činnost (e.g. sečti, znásob, etc.), nebo operativní příkaz.\n");
+                            break;
                         default:
                             Console.WriteLine("\nChuligáne! Tomu říkáš číslo!? Nemel blbosti a napiš něco pořádného!" + input + " konvertováno nebylo. \n");
                             break;
@@ -169,14 +176,30 @@ namespace Calculator
                 }
                 if (counter == 3)
                 {
-                    Console.WriteLine("\nPřevést do nějaké soustavy?\nAno/Ne? V případě ne ukončuji kód");
+                    Console.WriteLine("\nPřevést do nějaké soustavy?\nAno/Ne? V případě ne ukončuji kód\n(Note:Číslo bude převedeno na int, tudíž bude zaokrouhleno na nejbližší celé číslo\n");
                     input = Console.ReadLine();
                     if (input == "Ano")
                     {
-                        Console.WriteLine("Do které? K dispozici (1-32)");
-                        baseChange = Convert.ToInt32(Console.ReadLine());
-                        Convert.ToInt32(result);
-                        Convert.ToString(result, baseChange);
+                        Console.WriteLine("\nDo které? K dispozici (1-16)\n"); //Jen já a bůh víme jak tohle funguje
+                        input = Console.ReadLine();
+                        jeToInput = int.TryParse(input, out bases); //nový bool podle kterého určíme zda se jedná o číslo nebo nějakou blbost, pokud to není blbost, nastavíme jako betU (funkce out) (zkopírován oz mého deathrollu)
+                        if (jeToInput)
+                        {
+                            storage = Convert.ToInt32(result);
+                            Console.WriteLine("\nVýsledek:\n" + Convert.ToString(storage, bases) + "\n");//teď už jen bůh (Ne, vezme to int ve storage, a převede to na string tak aby to bylo ve 'bases' soustavě
+                        }
+                        else
+                        {
+                            if (input == "help")
+                            {
+                                HelpPrikaz();
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nChuligáne! Tomu říkáš číslo!? Nemel blbosti a napiš něco pořádného!" + input + " konvertováno nebylo. \n"); //vybídka ke znovunastavení hodnoty uživatelem
+                            }
+                        }
+
                     }
                     else if (input != "Ne")
                     {
