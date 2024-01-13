55031f0e7f367adea3f2e108d3b1b85d4d4fffc5
Tvojemamanaentou <cipherbill197@gmail.com>
2024-01-13 22:59:48 +0100

    polda v podstatě hotový! už jen touchups

--- HraEscape/HraEscape/Program.cs
+++ HraEscape/HraEscape/Program.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.Design;
-using System.Linq;
-using System.Text;
+using System.Diagnostics.Eventing.Reader;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Xml;
 
 namespace HraEscape
 {
-    /*Je to tu strašně chaotický ale přísahám bohu že to dává smysl:
+    /*Je to tu strašně chaotický ale přísahám bohu že to dává smysl. Spolužačka se o tom vyjádřila že to není spaghetti kód, ale tortellini kód protože je tam strašně moc úseků:
      * Jsou tu funkce '_Speaks' což jsou jen způsoby vypisování do konzole jinými estetickými vzhledy
      * Dále jsou tu funkce Error, což je... error a Help což je... já nevím jako jestli tohle mám vysvětlovat ono je to docela sebeevidentní....
      * Dále WhatNow která se sepne pokáždé když se "vejde" do nějaké lokace a poté se ptá hráče co chce v lokaci dělat
@@ -21,8 +15,9 @@ namespace HraEscape
     internal class Program
     {
         static int location = 0;
+        static int unterlocation = 0;
         static bool[] invState = new bool[5];
-        static string[] invText = { "hodně spravované UV světelko", "miska slunečnicových semínek", "gravírovaná bronzová růže", "potrhaný papírek", "zlatý klíček" };
+        static string[] invText = { "hodně spravované UV světelko", "miska slunečnicových semínek", "gravírovaná bronzová růže", "cár papíru", "zlatý klíček" };
         /*Máme pět itemů které může Pankrác použít. To jestli hráč má nějaký item řešíme tak že ukládáme bool na pozici itemu. a pro jeho vypsání pak porovnáváme s arrayem invText kde jsou uloženy jména itemů.
                                     * itemy:
                                     *0 - hodně spravované UV světelko
@@ -33,14 +28,14 @@ namespace HraEscape
                                     */
 
 
-        static void InventoryTextBuilder() //pětkrát opakujeme že koukneme jestli bool pole invState na pozici i-1 (abychom mohli použít na indexování v outputu) je pravda. Pokud ano, vypíšeme pozici z invText. Pokud ne, píšeme -nic
+        static void InventoryTextBuilder() //pětkrát opakujeme že koukneme jestli bool pole invState na pozici i je pravda. Pokud ano, vypíšeme pozici z invText. Pokud ne, píšeme -nic
         {
             string memory = "Inventář:";
-            for (int i = 1; i < 6; i++)
+            for (int i = 0; i < 5; i++)
             {
-                if (invState[i - 1] == true)
+                if (invState[i] == true)
                 {
-                    memory += "\n  " + i + "- " + invText[i - 1];
+                    memory += "\n  " + i + "- " + invText[i];
                 }
                 else
                 {
@@ -83,10 +78,10 @@ namespace HraEscape
             Console.WriteLine(speech);
             Console.ForegroundColor = ConsoleColor.White;
         }
-        static void Action(string input)
+        static void Action(string input, int location)
         {
             int memory = 0;
-            EventSpeaks("Jde o akci:\n1- holou rukou s lokací\n2- věcí(z inventáře) s lokací\n3- věcí s věcí");
+            EventSpeaks("Jde o akci:\n1- věcí(z inventáře) s lokací\n2- věcí s věcí");
             while (memory == 0)
             {
                 int.TryParse(Console.ReadLine(), out memory);
@@ -94,26 +89,95 @@ namespace HraEscape
             switch (memory)
             {
                 case 1:
-                    switch (location)
+                    int item = 6;
+                    EventSpeaks("Kterou věcí chceš interagovat?");
+                    while (item == 6)
                     {
-                        case 1: //u skříně 
-                            break;
-                        case 2: //u klece
-                            break;
-                        case 3: //u stolu
-                            while (location == 3)
+                        if (int.TryParse(Console.ReadLine(), out item))
+                        {
+                            if (item > 4 || item < -1)
                             {
-                                WhatNow(input);
+                                EventSpeaks("Vždyť tolik kapes ani nemam... Zkus něco jiného.\n");
+                                item = 6;
                             }
-                            break;
-                        case 4: //u knihovny
-                            break;
+                            else if (invState[item] == false)
+                            {
+                                EventSpeaks("Vždyť v téhle kapse ani nic nemam! Zkus něco jiného.\n");
+                                item = 6;
+                            }
+                            else
+                            {
+                                switch (location)
+                                {
+                                    case 1:
+                                        break;
+                                    case 2:
+                                        if (item == 1)
+                                        {
+                                            PankracSpeaks("Ná puťa puťa papoušek, dej si semínka!\n");
+                                            NarratorSpeaks("Papoušek upustil bronzovou růži\n");
+                                            PankracSpeaks("Ta se mi bude hodit, vezmu si ji!\n");
+                                            invState[1] = false;
+                                            invState[2] = true;
+                                        }
+                                        else
+                                        {
+                                            PankracSpeaks("Teda co s timhle tady mam vykoumat to netuším...");
+                                        }
+                                        break;
+                                    case 3:
+                                        break;
+                                    case 4:
+                                        switch (unterlocation)
+                                        {
+                                            case 1:
+                                                if (item == 2)
+                                                {
+                                                    PankracSpeaks("Zkusím tomu tanečníkovi tu bronzovou růži vložit do úst.\n");
+                                                    NarratorSpeaks("Z pod sošky se vykutálela rulička\n");  
+                                                    PankracSpeaks("Co to jé? Nějaká rulička papíru? Píše se na ní něco? Hm, nic. Co teď?\n");
+                                                    invState[2] = false;
+                                                    invState[3] = true;
+                                                }
+                                                else
+                                                {
+                                                    PankracSpeaks("Teda co s timhle tady mam vykoumat to netuším...");
+                                                }
+                                                break;
+                                            case 2:
+                                                if (invState[0] == false && item == 3) //víme, že hráč objevil kód, protože se po objevení kódu rozbije UV světlo.
+                                                {
+                                                    PankracSpeaks("Zkusím tam zadat ta písmenka z toho cáru papíru, třeba se otveře. Otevřel!\n");
+                                                    NarratorSpeaks("V sejfu byl zlatý klíček\n");
+                                                    PankracSpeaks("Klíček! Asi tuším, od čeho bude. ;)\n");
+                                                    invState[4] = true;
+                                                }
+                                                else
+                                                {
+                                                    PankracSpeaks("Teda co s timhle tady mam vykoumat to netuším...");
+                                                }
+                                                break;
+                                        }
+                                        break;
+                                }
+                                
+                            }
+                            
+                        }
+                        else
+                        {
+                            Error();
+                        }
+                        
                     }
+                    
                     break;
                 case 2:
-                    break;
-                case 3:
                     InterInventoryAction();
+                    break;
+                default:
+                    EventSpeaks("Há há, moc vtipný, teď zkus něco pořádnýho.");
+                    Action(input, location);
                     break;
             }
         }
@@ -126,14 +190,14 @@ namespace HraEscape
             while (itemA == 6)
             {
                 int.TryParse(Console.ReadLine(),out itemA);
-                if (itemA > 4)
+                if (itemA > 4 || itemA < 0)
                 {
-                    PankracSpeaks("Vždyť tolik kapes ani nemam... Zkus něco jiného.");
+                    PankracSpeaks("Vždyť tolik kapes ani nemam... Zkus něco jiného.\n");
                     itemA = 6;
                 }
                 else if (invState[itemA] == false)
                 {
-                    PankracSpeaks("Vždyť v téhle kapse ani nic nemam! Zkus něco jiného.");
+                    PankracSpeaks("Vždyť v téhle kapse ani nic nemam! Zkus něco jiného.\n");
                     itemA = 6;
                 }
             }
@@ -141,14 +205,14 @@ namespace HraEscape
             while (itemB == 6)
             {
                 int.TryParse(Console.ReadLine(), out itemB);
-                if (itemB > 4)
+                if (itemB > 4 || itemB < 0)
                 {
-                    PankracSpeaks("Vždyť tolik kapes ani nemam... Zkus něco jiného.");
+                    PankracSpeaks("Vždyť tolik kapes ani nemam... Zkus něco jiného.\n");
                     itemB = 6;
                 }
                 else if (invState[itemB] == false)
                 {
-                    PankracSpeaks("Vždyť v téhle kapse ani nic nemam! Zkus něco jiného.");
+                    PankracSpeaks("Vždyť v téhle kapse ani nic nemam! Zkus něco jiného.\n");
                     itemB = 6;
                 }
             }
@@ -157,7 +221,8 @@ namespace HraEscape
             {
                 PankracSpeaks("JŮ! On ten útržek úplně začal svítit! No ale co to-? Něco se tu píše. Ká eR eL É? Co to znamená? Že by Krleš? Ale co to má s Kriste- JAÚ! Jéžiš! Ten zpropadenej UV zmetek mě kopnul a já ho pustil. Tak teď už ho opravdu nezachráním");
                 invState[0] = false;
-            }
+                invText[3] = "Cár papíru s písmeny K R L E";
+             }
             else
             {
                 PankracSpeaks("Teda co s těmahle věcma mam vykoumat to netuším...");
@@ -208,23 +273,17 @@ namespace HraEscape
                 switch (input)
                 {
                     case "knihy":
-                        PankracSpeaks("Je tu nějaký dopis! 'Milý Pankráci. Omluv prosím nepořádek, musel jsem odejít ve spěchu. Jdou po mě. Další stopa je ve skříni.' No to jsem z toho Jelen... Chudák Alfréd. Takže další stopa je ve skříni. No ale, jak se do ní dostanu?\n");
+                        PankracSpeaks("Plno knih... nějaký Jirka Kegel, Lakán, Houmr a dokonce i Džín Žák Rusák! Ale s případem mi nic z toho nepomůže\n");
+                        WhatNow(input);
                         break;
                     case "soška":
-                        PankracSpeaks("Obyčejný hrnek s pozlaceným okrajem. Nic Zvláštního...\n");
+                        PankracSpeaks("Bronzová soška tančícího páru. Ale že MÁ ten chlapík divně zkroucenou hubu! Skoro jako kdyby mu v ní něco chybělo.\n");
+                        unterlocation = 1;
+                        WhatNow(input);
                         break;
                     case "sejf":
-                        if (invState[0] == false) //víme, že hráč objevil kód, protože se po objevení kódu rozbije světlo.
-                        {
-                            PankracSpeaks("Zkusím tam zadat ta písmenka z toho cáru papíru, třeba se otveře. Otevřel!\n");
-                            NarratorSpeaks("V sejfu byl zlatý klíček");
-                            PankracSpeaks("Klíček! Asi tuším, od čeho bude. ;)");
-                            invState[3] = true;
-                        }
-                        else
-                        {
-                            PankracSpeaks("");
-                        }
+                        PankracSpeaks("Bytelný litinový sejf. Do něj si nejspíš Alfréd schovává své cenosti.\n");
+                        unterlocation = 2;
                         break;
                     default:
                         if (input == "inv" || input == "pomoc" || input == "akce" || input == "zpět")
@@ -239,7 +298,6 @@ namespace HraEscape
                 }
             }
         }
-
         static void WhatNow(string input)
         {
             if (input == "inv" || input == "pomoc" || input == "akce" || input == "zpět")
@@ -255,7 +313,7 @@ namespace HraEscape
                         input = "";
                         break;
                     case "akce":
-                        Action(input);
+                        Action(input, location);
                         input = "";
                         break;
                     case "zpět":
@@ -287,12 +345,6 @@ namespace HraEscape
 
 
 
-
-
-
-
-
-
         static void Main(string[] args)
         {
             invState[0] = true; //hráč začíná s UV světlem 
@@ -307,6 +359,7 @@ namespace HraEscape
                 {
                     case "skříň":
                         location = 1;
+                        PankracSpeaks("Pěkná malovaná skříň. Zajímalo by mě, jestli má Lord Alfréd také zevnitř na dveřích nějaké prasečinky hehehé!\n");
                         while (location == 1)
                         {
                             WhatNow(input);
@@ -314,6 +367,7 @@ namespace HraEscape
                         break;
                     case "klec":
                         location = 2;
+                        PankracSpeaks("Jéjé! Nevděl jsem, že Alfréd chová papouška! To je ale fešák! Ale cosi to má v puse. Jakousi bronzovou růži.\n");
                         while (location == 2)
                         {
                             WhatNow(input);
@@ -323,16 +377,20 @@ namespace HraEscape
                         location = 3;
                         NarratorSpeaks("Na stole ležely tři věci.");
                         PankracSpeaks("Hm, dopis, miska a šálek s ještě teplou nedopitou kávou. To tu asi musel nechat Alfréd.");
-                        EventSpeaks("Podlokace:\ndopis\nhrnek\nmiska");
+                        EventSpeaks("zPodlokace:\ndopis\nhrnek\nmiska");
                         while (location == 3)
                         {
-                            
                             LocationTable(input);
                         }
                         break;
                     case "knihovna":
                         location = 4;
-                        WhatNow(input);
+                        NarratorSpeaks("V knihovně bylo plno knížek (kdo by to byl čekal), ale také sejf a bronzová soška.");
+                        EventSpeaks("Podlokace:\nsejf\nsoška\nknihy");
+                        while (location == 4)
+                        {
+                            LocationBookcase(input);
+                        }
                         break;
                     default:
                         Error();
